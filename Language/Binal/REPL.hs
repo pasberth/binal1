d83f47d21d0d34abe8d3e841013d8f38ff00c56e
pasberth <pasberth@gmail.com>
2014-05-26 10:40:14 +0900

    REPL で状態持つようにした

--- Language/Binal/REPL.hs
+++ Language/Binal/REPL.hs
@@ -1,32 +1,54 @@
 module Language.Binal.REPL where
 
-import           Control.Monad.Trans
+import           Control.Monad.State
+import qualified Data.List                 as List
+import qualified Data.HashSet              as HashSet
+import qualified Data.IORef                as IORef
 import qualified System.Console.Haskeline  as Haskeline
 import qualified Language.Binal.Util       as Util
+import qualified Language.Binal.PrettyPrint as PP
 import qualified Language.Binal.Parser     as P
-import qualified Language.Binal.CLI        as CLI
+import qualified Language.Binal.Verifier as V
 
 repl :: IO ()
-repl = Haskeline.runInputT Haskeline.defaultSettings loop where
-  loop :: Haskeline.InputT IO ()
-  loop = do
+repl = Haskeline.runInputT Haskeline.defaultSettings start where
+  start = do
+    ref1 <- liftIO (IORef.newIORef HashSet.empty)
+    ref2 <- liftIO (IORef.newIORef (Util.initialTypeEnv, Util.initialVarList, [], Util.initialPolyEnv))
+    loop ref1 ref2
+  loop ref1 ref2 = do
     minput <- Haskeline.getInputLine "Binal < "
     case minput of
       Nothing -> return ()
       Just ":q" -> return ()
       Just input
-        | null input -> loop
+        | null input -> loop ref1 ref2
         | otherwise -> do
           maybeAST <- liftIO (P.parseStringSExp (input ++ "\n"))
           case maybeAST of
             Just ast -> do
-              maybeTypedAST <- liftIO (CLI.checkAST ast)
-              case maybeTypedAST of
-                Just typedAST -> do
-                  let msg = "- : " ++ Util.showTy (Util.typeof typedAST)
-                  Haskeline.outputStrLn msg
-                Nothing -> do
-                  return ()
-            Nothing -> do
-              return ()
-          loop
+              case V.examineForms ast of
+                [] -> do
+                  state1 <- liftIO (IORef.readIORef ref1)
+                  case runState (V.examineNames' ast) state1 of
+                    ([], newState1) -> do
+                      state2 <- liftIO (IORef.readIORef ref2)
+
+                      let (typedAST, newState2@(_, _, constraints, _)) = runState (V.inferType' ast) state2
+                      let absurds = List.nub (V.cantUnify (reverse constraints))
+                      let typedAST' =  Util.mapTyKind (V.unify (reverse constraints) . Util.flatListTy) typedAST
+                      case absurds of
+                        [] -> do
+                          liftIO $ do
+                            IORef.writeIORef ref1 newState1
+                            IORef.writeIORef ref2 newState2
+                          let msg = "- : " ++ Util.showTy (Util.typeof typedAST')
+                          Haskeline.outputStrLn msg
+                        errs -> do
+                          liftIO (mapM_ PP.prettyANSIAbsurd errs)
+                    (errs, _) -> do
+                      liftIO (mapM_ PP.prettyANSINotInScope errs)
+                errs -> do
+                  liftIO (mapM_ PP.prettyANSISyntaxError errs)
+            Nothing -> return ()
+          loop ref1 ref2
